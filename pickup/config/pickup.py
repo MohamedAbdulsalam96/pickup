9dba1595c9a0ea82f91f710de40d8dcd797ea9b3
britlog <info@britlog.com>
2020-12-09 22:02:13 +0100

    Add order form report

---
+++ pickup/config/pickup.py
@@ -0,0 +1,39 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Slots"),
+			"icon": "fa fa-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Pickup Point",
+					"description": _("Pickup Points."),
+				},
+				{
+					"type": "doctype",
+					"name": "Pickup Slot",
+					"description": _("Pickup Slots."),
+				}
+			]
+		},
+		{
+			"label": _("Reports"),
+			"icon": "fa fa-list",
+			"items": [
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Order Form"
+				},
+			]
+		},
+		{
+			"label": _("Setup"),
+			"icon": "fa fa-cog",
+			"items": [
+			]
+		},
+	]
